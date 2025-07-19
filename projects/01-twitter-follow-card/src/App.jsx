c334920a7f49d3e8b3dde59a56c711b79963ad6f
Gael <gaelfranciscoM.47@gmail.com>
2025-07-19 03:15:05 -0600

    proyecto uno

---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,19 @@
+export function App () {
+    return (
+        <article>
+            <header>
+                <img alt="Avatar de midu" src="https://unavatar.io/midudev"/>
+                <div>
+                    <strong>Miguel Angel Duran</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+            
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
