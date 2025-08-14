d725e1396f0f154b633d526066b6a7cbe545efb8
Gael <gaelfranciscoM.47@gmail.com>
2025-08-14 11:16:52 -0600

    button and useEffect

--- projects/prueba-tecnica-react/src/App.jsx
+++ projects/prueba-tecnica-react/src/App.jsx
@@ -1,37 +1,50 @@
 import { useEffect, useState } from 'react'
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-//const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/'
-//const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWords}$?fontSize=50&fontColor=red&json=true`
 
 export function App() {
     const [fact, setFact] = useState()
     const [imageUrl, setImageUrl] = useState()
-
+    const getRandomFact = () => {
+      fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res => res.json())
+        .then(data => {
+          const { fact } = data 
+          setFact(fact)
+        })
+    }
+    // Cargar un hecho aleatorio al iniciar la app
+    useEffect(getRandomFact, [])
+    // Cargar una imagen de gato al cambiar el hecho
     useEffect(() => {
-        fetch(CAT_ENDPOINT_RANDOM_FACT)
-            .then(res => res.json())
-            .then(data => {
-                const { fact } = data 
-                setFact(fact)
+      if (!fact) return
 
-                const threeFirstWords = fact.split(' ', 3).join(' ')
-                console.log(threeFirstWords)
+      const threeFirstWords = fact.split(' ', 3).join(' ')
+      console.log(threeFirstWords)
 
-                fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-                    .then(res => res.json())
-                    .then(response => {
-                        const { url } = response
-                        setImageUrl(`https://cataas.com${url}`)
-                    })
-            })
-        }, [])
+      fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
+        .then(res => res.json())
+        .then(response => {
+          const { url } = response
+          setImageUrl(url)
+        })
+    }, [fact])
+
+  const handleClick = () => {
+    getRandomFact()
+  }
 
   return (
     <main>
-      <h1>App de gatitos 🐱</h1>
-      {fact && <p>{fact}</p>}
-      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+        <h1>App de gatitos 🐱</h1>
+      
+      <button onClick={handleClick}>Get new fact</button>
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+      </section>
     </main>
   )
 }
